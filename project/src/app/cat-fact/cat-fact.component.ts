400ea92ec294b80a272097a99f6c0bf1a921f3d5
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-11-11 09:03:04 +0200

    implement a fetch to cat-facts api also

--- project/src/app/cat-fact/cat-fact.component.ts
+++ project/src/app/cat-fact/cat-fact.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-cat-fact',
   templateUrl: './cat-fact.component.html',
   styleUrls: ['./cat-fact.component.css']
 })
-export class CatFactComponent {
+export class CatFactComponent implements OnInit {
+  catFact: string;
+  catFactCreatedAt: string;
 
+  constructor(private http: HttpClient) {
+    this.catFact = 'Loading...';
+    this.catFactCreatedAt = 'Loading...';
+  }
+
+  ngOnInit(): void {
+    this.fetchCatFact();
+  }
+
+  fetchCatFact() {
+    const apiUrl = 'https://cat-fact.herokuapp.com/facts/58e00b5f0aac31001185ed24';
+
+    this.http.get(apiUrl).subscribe((data: any) => {
+      this.catFact = data.text;
+      this.catFactCreatedAt = data.createdAt;
+    });
+  }
 }
