7e1cf743ea3c15c01bf7efc3eae9eb1a7db8bdf6
Arvydas <78803192+arvydasg@users.noreply.github.com>
2023-11-11 08:51:02 +0200

    created cat-fact component

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TimeDisplayComponent } from './time-display/time-display.component';
+import { CatFactComponent } from './cat-fact/cat-fact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TimeDisplayComponent
+    TimeDisplayComponent,
+    CatFactComponent
   ],
   imports: [
     BrowserModule,
---
+++ project/src/app/cat-fact/cat-fact.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-cat-fact',
+  templateUrl: './cat-fact.component.html',
+  styleUrls: ['./cat-fact.component.css']
+})
+export class CatFactComponent {
+
+}
